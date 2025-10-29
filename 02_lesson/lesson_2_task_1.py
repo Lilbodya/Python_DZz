057f10f2c40aefe37d0efb55accec00ac9c6b3c7
Egor <gorshenin.egor2015@yandex.ru>
2025-10-29 18:32:56 +0300

    Delete 02_lesson directory

--- 02_lesson/lesson_2_task_1.py
+++
@@ -1,7 +0,0 @@
-lst = ["Виноград", "Клубника", "Груша", "Апельсин", "Банан", "Яблоко"]
-
-first_fruit = lst[0]
-last_fruit = lst[-1]
-
-print(f"Первый фрукт: {first_fruit}")
-print(f"Последний фрукт: {last_fruit}")
